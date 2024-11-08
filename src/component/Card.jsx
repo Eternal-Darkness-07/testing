f493a96e1cc5a63fb25c79f713781a905401519c
Eternal-Darkness-07 <varshakotadiya1980@gmail.com>
2024-11-08 20:21:24 +0530

    update cards

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'; // Ensure this CSS file is included for styling
 const Card = ({ image, name, description }) => {
   return (
     <div className="card-container">
-      <img src={image} alt="Profile" className="card-img-top" />
+      {/* <img src={image} alt="Profile" className="card-img-top" /> */}
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
